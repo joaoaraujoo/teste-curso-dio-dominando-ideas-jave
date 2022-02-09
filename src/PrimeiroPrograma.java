6af389dc5677c44fb1fd86a208f434164ec5bfe3
João Araujo <joausv@outlook.com>
2022-02-09 01:46:28 -0300

    first commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+    
+		Gato gato = new Gato ();
+		
+		System.out.print1n(gato);
+	}
+
+}
+
+
+
+
